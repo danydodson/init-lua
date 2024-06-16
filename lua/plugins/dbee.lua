ff13577ace0cfaa224d5e7016c04e9ac9ecc8a5d
danydodson <danydodson@gmail.com>
2024-06-16 02:09:40 -0500

    chore: Update dbee.lua with new database source configuration

--- lua/plugins/dbee.lua
+++ lua/plugins/dbee.lua
@@ -1,27 +1,30 @@
 -- https://github.com/kndndrj/nvim-dbee
 
+-- For now, I'm going to stick with dadbod,
+-- but if the completion continues to improve I will probably switch
 return {
   {
     "kndndrj/nvim-dbee",
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-    },
+    enabled = false,
+    dependencies = { "MunifTanjim/nui.nvim" },
     build = function()
-      -- Install tries to automatically detect the install method.
-      -- if it fails, try calling it with one of these parameters:
-      --    "curl", "wget", "bitsadmin", "go"
       require("dbee").install()
     end,
     config = function()
-      -- require("dbee").setup( --[[optional config]])
-
-      require("dbee.sources").MemorySource:new({
-        {
-          name = "...",
-          type = "...",
-          url = "...",
-        }
-      })
+      local source = require "dbee.sources"
+      require("dbee").setup {
+        sources = {
+          source.MemorySource:new({
+            ---@diagnostic disable-next-line: missing-fields
+            {
+              type = "postgres",
+              name = "mixery",
+              url = "postgresql://tjdevries:password@localhost:5432/mixery",
+            },
+          }, "mixery"),
+        },
+      }
+      require "custom.dbee"
     end,
   },
 }
